818993780727be9c5905d79ab8688f114b9896d7
arjunraichura0502-boop <arjunraichura0502@gmail.com>
2026-04-25 21:24:40 +0530

    Update Intro.java

--- Intro.java
+++ Intro.java
@@ -2,6 +2,14 @@ import java.util.*;
 public class Intro
   {
     public static void main(String[] args){
-      System.out.println("Hello World");
+        int a,b,Hello;
+        Scanner A =new scanner(System.in);
+Scanner B =new scanner(System.in);
+System.out.println("Enter the first number =");
+a =A.nextInt();
+System.out.println("Enter the second number =");
+b=B.nextInt();
+Hello=a+b;
+      System.out.println("Hello World "+Hello);
     }
   }
